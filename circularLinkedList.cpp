e42ed4639c263fe87ea2861ddc47dff8c0027a82
Mohit Kuamr <mohitk2k1@gmail.com>
2024-04-03 11:09:08 +0530

    Adding the deletion functionality in Circular Linked List

--- circularLinkedList.cpp
+++ circularLinkedList.cpp
@@ -51,6 +51,48 @@ void insertAtTail(node *&head, int val)
     n->next = head;
 }
 
+void deletionAtTail(node* &head){
+    node* temp = head;
+    while (temp->next->next != head){
+        temp = temp->next;
+    }
+    node* toDelete = temp->next;
+    delete(toDelete);
+    temp->next = head;
+    
+    
+}
+
+void deletionAtHead(node* &head){
+    node* temp = head;
+    while(temp->next!=head){
+        temp = temp->next;
+    }
+    node* todelete = temp->next;
+    head = temp->next->next;
+    temp->next = head;
+    delete(todelete);
+}
+
+void deletionAtPosition(node* &head, int pos){
+    node* temp = head;
+    if(pos==0){
+        deletionAtHead(head);
+    }
+    int count = 0;
+    while(count!=pos-1){
+        temp = temp->next;
+        count++;
+    }
+
+    node * todelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete todelete;
+
+
+}
+
 void display(node *head)
 {
     node *temp = head;
@@ -65,12 +107,28 @@ void display(node *head)
 int main()
 {
     node *head = nullptr;
+    // Insertion at tail 
+    cout<<"Insertion at head: "<<endl;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
     insertAtTail(head, 3);
     insertAtTail(head, 4);
     display(head);
+    // Insertion at head
+    cout<<"Insertion at head: "<<endl;
     insertAtHead(head, 5);
+    display(head);
+    // Deletion at tail
+    // cout<<"Deletion at tail: "<<endl;
+    // deletionAtTail(head);
+    // display(head);
+    // cout<<"Deletion at head: "<<endl;
+    // deletionAtHead(head);
+    // display(head);
+
+    // Deletion at given position 
+    cout<<"Deletion at given postion: "<<endl;
+    deletionAtPosition(head,3);
     display(head);
 
     return 0;
