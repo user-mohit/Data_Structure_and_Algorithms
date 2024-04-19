e7cfe6668b78e2fe3afc1cfe6f2fd88975964e87
Mohit Kuamr <mohitk2k1@gmail.com>
2024-04-19 14:28:58 +0530

    Insertion sort

---
+++ insertionSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    int key, j;
+    for (int i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+
+    int arr[] = {78, 15, 56, 45, 12, 1};
+    int n = 6;
+    insertionSort(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
