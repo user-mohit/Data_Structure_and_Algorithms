a3ac753f6ad55f4e3df3c1c7235d9148d166771b
Mohit Kuamr <mohitk2k1@gmail.com>
2024-05-17 12:45:41 +0530

    Adding searching in BST

--- searchign_in_BST.cpp
+++ searchign_in_BST.cpp
@@ -1,8 +1,78 @@
-#include<iostream>
-using namespace std ;
+#include <iostream>
+using namespace std;
 
-int main() 
+struct Node
 {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// INSERTION IN BINARY SEARCH TREE
+Node *insert(Node *root, int val)
+{
+    if (root == nullptr)
+    {
+        return new Node(val);
+    }
+    if (root->data > val)
+    {
+        root->left = insert(root->left, val);
+    }
+    else
+    {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+Node *search(Node *root, int key)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->data == key)
+    {
+        return root;
+    }
+    if (key < root->data)
+    {
+        return search(root->left, key);
+    }
+    else
+    {
+        return search(root->right, key);
+    }
+}
+
+int main()
+{
+    Node *root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    Node *n = search(root, 90);
+    if (n != NULL)
+    {
+        cout <<"Found : "<< n->data << endl;
+    }
+    else
+    {
+        cout << "Element not found!" << endl;
+    }
 
     return 0;
 }
